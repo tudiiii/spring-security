5dc404fbd25705687568ba8acfa00ea390c87835
tudy <jhh0207@hanmail.net>
2023-02-06 23:25:19 +0900

    init project

---
+++ src/test/java/me/tudy/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tudy.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
